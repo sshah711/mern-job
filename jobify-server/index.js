214be9363a356393242d16cd7f808d5065fbae4f
sshah711 <sshah380008@gmail.com>
2024-02-09 08:14:04 +0530

    updatepage done

--- jobify-server/index.js
+++ jobify-server/index.js
@@ -17,87 +17,112 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.DATABASE;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    //create Db
-    const db= client.db("Jobify");
-    const jobsCollections = db.collection("demoJobs");
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        //create Db
+        const db = client.db("Jobify");
+        const jobsCollections = db.collection("demoJobs");
 
-    //post jobs
-    app.post("/post-job", async(req,res) => {
-        const body = req.body;
-        body.createAt = new Date();
-        //console.log(body);
-        const result = await jobsCollections.insertOne(body);
-        if(result.insertedId){
-            return res.status(200).send(result);
-        }else{
-            return res.status(404).send({
-                message: "can not insert!! try again later",
-                status: false
+        //post jobs
+        app.post("/post-job", async (req, res) => {
+            const body = req.body;
+            body.createAt = new Date();
+            //console.log(body);
+            const result = await jobsCollections.insertOne(body);
+            if (result.insertedId) {
+                return res.status(200).send(result);
+            } else {
+                return res.status(404).send({
+                    message: "can not insert!! try again later",
+                    status: false
+                })
+            }
+        })
+
+
+        //get all jobs
+        app.get("/all-jobs", async (req, res) => {
+            const jobs = await jobsCollections.find({}).toArray();
+            res.send(jobs);
+        })
+
+        //get single job using id
+        app.get("/all-jobs/:id", async (req, res) => {
+            const id = req.params.id;
+            const job = await jobsCollections.findOne({
+                _id: new ObjectId(id)
             })
-        }
-    } )
+            res.send(job);
+        })
 
 
-    //get all jobs
-    app.get("/all-jobs", async(req,res) => {
-        const jobs= await jobsCollections.find({}).toArray();
-        res.send(jobs);
-    } )
+        //get jobs by email
+        app.get("/my-job/:email", async (req, res) => {
+            //console.log(req.params.email)
+            const jobs = await jobsCollections.find({ postedBy: req.params.email }).toArray();
+            res.send(jobs);
+        })
 
-    //get jobs by email
-    app.get("/my-job/:email", async(req,res) => {
-        //console.log(req.params.email)
-         const jobs= await jobsCollections.find({postedBy : req.params.email}).toArray();
-         res.send(jobs);
-    } )
-
-    //delete a job
-    app.delete("/job/:id", async(req,res) => {
-       const id = req.params.id;
-       const filter = {_id: new ObjectId(id)}
-       const result = await jobsCollections.deleteOne(filter);
-       res.send(result);
-    } )
+        //delete a job
+        app.delete("/job/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const result = await jobsCollections.deleteOne(filter);
+            res.send(result);
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+        //update a job
+        app.patch("/update-job/:id", async (req, res) => {
+            const id = req.params.id;
+            const jobData = req.body;
+            const filter = { _id: new ObjectId(id) }
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    ...jobData
+                },
+            };
+            const result = await jobsCollections.updateOne(filter, updateDoc, option);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello Developer')
+    res.send('Hello Developer')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 
 // const express = require('express')
 // const app = express()
 
-// //load config from env file 
+// //load config from env file
 // require("dotenv").config()
-// const PORT = process.env.PORT 
+// const PORT = process.env.PORT
 
 // // middleware to parse json request body
 // app.use(express.json())
